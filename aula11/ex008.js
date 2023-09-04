cdfca1f113d332b9976434c2efb94d7400420216
BernardoFeldmann <ancora237@gmail.com>
2023-09-04 17:55:56 -0300

    .

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 61
-console.log('A velocidade do seu carro é de ' + vel + 'Km/h' )
-if (vel > 60){
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+var vel = 50.2
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 50){ // Condição
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO! Você passou a ${vel}Km/h`)
 }
 console.log('Dirija sempre usando o cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,23 @@
+
+
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+}
+else {
+    console.log('Você é Estrangeiro')
+}
+
+//      OU 
+
+console.log('///////////////////////////////')
+
+var país = 'EUA'
+console.log(`Vivendo em ${país}`)
+if (país !='Brasil') {
+    console.log('Você é Estrangeiro!')
+}
+else {
+    console.log('Você é Brasileiro')
+}
