01dc4f204e0647f4f85edf364925155ccf8676a3
BernardoFeldmann <ancora237@gmail.com>
2023-09-18 18:02:55 -0300

    .

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,22 +1,22 @@
-
+/*
 var c = 1
 do {
     console.log(`REPETIR ${c}`)
     c++
 } while (c <= 3)
+*/
 
-
-/*
 var c = 1
 while(c <= 3) {
     console.log(`REPETIR ${c}`)
     c +=1
 }
 
+/*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
-*/
+*/ 
