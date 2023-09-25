cb0be04c6fd6525ae3545c8c61877e2d9b3b082e
Bernardo Feldmann <ancora237@gmail.com>
2023-09-25 13:49:43 -0300

    .

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,18 @@
-function carregar() {
-    
+function gerar() {
+    var num = document.getElementById('idnum')
+    var tabuada = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        var n = Number(num.value)
+         
+        var c = 1
+        tabuada.innerHTML='' // limpa tabuada, para proximo numero
+        while(c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tabuada.appendChild(item)
+            c++
+        }
+    }
 }
