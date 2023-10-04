31c17d144d99b07463a323fbcc6437af070fb354
BernardoFeldmann <ancora237@gmail.com>
2023-10-04 18:07:37 -0300

    .

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,12 @@
 var num = [5,6,9,10,12]
+num.push(1)
 num.sort
 console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+var pos = num.indexOf(3)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,5 +1,11 @@
-var valores = [8, 5, 10, 15]
+/*var valores = [8, 5, 10, 15]
 
-for (var pos = 0; pos < valores.length; pos ++) { // enquanto não chegar no último valor...
+for (var pos = 0; pos < valores.length; pos ++) { // enquanto não chegar no último valor, continua recebendo valores
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+var num = [4,9,7,2,1]
+num.sort()
+for(var pos in num) {
+    console.log(`na posição ${pos} tem o valor ${num[pos]}`)
 }
